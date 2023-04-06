8824ab168bda8b9691a2b41589ca0a3627c6918c
manuetov <manuetov@hotmail.com>
2023-04-06 18:43:13 +0200

    implemented security with user & roles

--- src/main/java/com/blog/controller/PostBlogController.java
+++ src/main/java/com/blog/controller/PostBlogController.java
@@ -6,6 +6,7 @@ import com.blog.service.PostBlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import jakarta.validation.Valid;
@@ -29,17 +30,20 @@ public class PostBlogController {
         return ResponseEntity.ok(postBlogService.getPostBlogById(id));
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PostMapping
-    public ResponseEntity<PostBlogDTO> savePost(@Valid @RequestBody PostBlogDTO postBlogDTO){
+    public ResponseEntity<PostBlogDTO> createPost(@Valid @RequestBody PostBlogDTO postBlogDTO){
         return new ResponseEntity<>(postBlogService.createPost(postBlogDTO), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @PutMapping("/{id}")
     public ResponseEntity<PostBlogDTO> updateSinglePost(@Valid @RequestBody PostBlogDTO postBlogDTO,
                                                         @PathVariable(name = "id") long id) {
         return new ResponseEntity<>(postBlogService.updatePostBlogById(postBlogDTO, id), HttpStatus.CREATED);
     }
 
+    @PreAuthorize("hasRole('ADMIN')")
     @DeleteMapping("/{id}")
     public ResponseEntity<HttpStatus> deleteSinglePost(@PathVariable(name = "id") long id) {
         postBlogService.deletePostBlogById(id);
